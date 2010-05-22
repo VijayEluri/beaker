c7145bc30de633a26848f2f3cbfefe2b4a5004c5
Jason Dillon <jason@planet57.com>
2010-05-22 14:20:21 -0700

    Tidy

--- beaker-lexicon/src/main/java/com/sonatype/beaker/lexicon/Generic.java
+++ beaker-lexicon/src/main/java/com/sonatype/beaker/lexicon/Generic.java
@@ -3,7 +3,9 @@ package com.sonatype.beaker.lexicon;
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 
 /**
- * ???
+ * Meep that wraps any {@link Object}.
+ *
+ * This should only be used for internal testing, debugging, as consumption of this meep may fail due to missing classes.
  *
  * @author <a href="mailto:jason@planet57.com">Jason Dillon</a>
  * @since 0.1
