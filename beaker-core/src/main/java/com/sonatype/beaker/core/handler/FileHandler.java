0fb83c921a5b6bf032ed3cd414e23cae2ff76da3
Jason Dillon <jason@planet57.com>
2010-05-24 00:07:50 -0700

    Slightly better tmp file name

--- beaker-core/src/main/java/com/sonatype/beaker/core/handler/FileHandler.java
+++ beaker-core/src/main/java/com/sonatype/beaker/core/handler/FileHandler.java
@@ -38,7 +38,7 @@ public class FileHandler
         String filename = System.getProperty(FILE);
         File file;
         if (filename == null) {
-            file = File.createTempFile("beaker", ".xmeep");
+            file = File.createTempFile("beaker-", ".xmeep");
         }
         else {
             file = new File(filename);
