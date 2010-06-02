9042ce46a868d2753ba5612d0a54dfd82ed26add
Jason Dillon <jason@planet57.com>
2010-06-01 18:02:00 -0700

    Add verbosity control

--- beaker-maven/beaker-maven-common/src/main/java/org/sonatype/beaker/maven/JoinPointTrace.java
+++ beaker-maven/beaker-maven-common/src/main/java/org/sonatype/beaker/maven/JoinPointTrace.java
@@ -1,11 +1,12 @@
 package org.sonatype.beaker.maven;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.Signature;
 
 import java.io.PrintStream;
 
 /**
- * ???
+ * Support for trace aspects.
  *
  * @author <a href="mailto:jason@planet57.com">Jason Dillon</a>
  * @since 0.1
@@ -14,32 +15,74 @@ public class JoinPointTrace
 {
     public static int TRACELEVEL = 2;
 
-    protected static PrintStream stream = System.err;
+    private static PrintStream stream = System.err;
 
-    protected static int callDepth = 0;
+    private static enum Verbosity
+    {
+        TERSE,
+        NORMAL,
+        VERBOSE;
 
-    public static void initStream(PrintStream s) {
-        stream = s;
+        public static Verbosity get() {
+            String value = System.getProperty("beaker.trace.verbosity");
+            if (value == null) {
+                return NORMAL;
+            }
+            else {
+                return valueOf(value.toUpperCase());
+            }
+        }
+    }
+
+    private static Verbosity verbosity = Verbosity.get();
+
+    private static int callDepth = 0;
+
+    public static void initStream(final PrintStream out) {
+        assert out != null;
+        stream = out;
     }
 
     public static void entry(final JoinPoint point) {
-        traceEntry("" + point.getSignature().toLongString());
+        traceEntry(render(point));
     }
 
     public static void exit(final JoinPoint point) {
-        traceExit("" + point.getSignature().toLongString());
+        traceExit(render(point));
     }
 
-    protected static void traceEntry(String str) {
-        if (TRACELEVEL == 0) return;
-        if (TRACELEVEL == 2) callDepth++;
+    private static String render(final JoinPoint point) {
+        assert point != null;
+        Signature sig = point.getSignature();
+        switch (verbosity) {
+            case TERSE:
+                return sig.toShortString();
+            case NORMAL:
+                return sig.toString();
+            case VERBOSE:
+                return sig.toLongString();
+        }
+        throw new Error();
+    }
+
+    private static void traceEntry(final String str) {
+        if (TRACELEVEL == 0) {
+            return;
+        }
+        if (TRACELEVEL == 2) {
+            callDepth++;
+        }
         printEntering(str);
     }
 
-    protected static void traceExit(final String str) {
-        if (TRACELEVEL == 0) return;
+    private static void traceExit(final String str) {
+        if (TRACELEVEL == 0) {
+            return;
+        }
         printExiting(str);
-        if (TRACELEVEL == 2) callDepth--;
+        if (TRACELEVEL == 2) {
+            callDepth--;
+        }
     }
 
     private static void printEntering(final String str) {
